8d656a4530c9472ea57d31af1df78fc73317de2e
Artur Weber <aweber@dithernet.org>
2023-01-30 19:51:27 +0100

    dts: add kylepro dts

--- arch/arm/mach-bcm/board_bcm21664.c
+++ arch/arm/mach-bcm/board_bcm21664.c
@@ -4,6 +4,14 @@
 #include <asm/mach/arch.h>
 
 #include "kona_l2_cache.h"
+#include <linux/irqchip.h>
+#include <soc/bcmkona-pwrmgr.h>
+
+static void __init bcm21664_irq_init(void)
+{
+	kona_pwrmgr_early_init();
+	irqchip_init();
+}
 
 static void __init bcm21664_init(void)
 {
@@ -16,6 +24,7 @@ static const char * const bcm21664_dt_compat[] = {
 };
 
 DT_MACHINE_START(BCM21664_DT, "BCM21664 Broadcom Application Processor")
+	.init_irq = bcm21664_irq_init,
 	.init_machine = bcm21664_init,
 	.dt_compat = bcm21664_dt_compat,
 MACHINE_END
