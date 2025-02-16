d4cc8a0337432d7ea90d7667cc114382df9b03be
Artur Weber <aweber.kernel@gmail.com>
2025-02-16 13:22:27 +0100

    clk: bcm21664: Add policy bit configuration for clocks
    The policy bits are required for gating to work correctly; without
    them set, gating init and clock enable/disable fails, unless the
    bootloader sets up the policy bits.
    
    Add policy bits to the clock configuration for the clocks that have
    them to allow gating to work correctly.
    
    Signed-off-by: Artur Weber <aweber.kernel@gmail.com>

--- drivers/clk/bcm/clk-bcm21664.c
+++ drivers/clk/bcm/clk-bcm21664.c
@@ -30,6 +30,7 @@ static struct ccu_data root_ccu_data = {
 /* AON CCU */
 
 static struct peri_clk_data hub_timer_data = {
+	.policy		= POLICY(0x0010, 6),
 	.gate		= HW_SW_GATE(0x0414, 16, 0, 1),
 	.hyst		= HYST(0x0414, 8, 9),
 	.clocks		= CLOCKS("bbl_32k",
@@ -40,6 +41,7 @@ static struct peri_clk_data hub_timer_data = {
 };
 
 static struct bus_clk_data hub_timer_apb_data = {
+	.policy		= POLICY(0x0010, 6),
 	.gate		= HW_SW_GATE(0x0414, 18, 3, 2),
 	.hyst		= HYST(0x0414, 10, 11),
 };
@@ -62,6 +64,7 @@ static struct ccu_data aon_ccu_data = {
 /* Master CCU */
 
 static struct peri_clk_data sdio1_data = {
+	.policy		= POLICY(0x0010, 6),
 	.gate		= HW_SW_GATE(0x0358, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_52m",
@@ -74,6 +77,7 @@ static struct peri_clk_data sdio1_data = {
 };
 
 static struct peri_clk_data sdio2_data = {
+	.policy		= POLICY(0x0010, 5),
 	.gate		= HW_SW_GATE(0x035c, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_52m",
@@ -86,6 +90,7 @@ static struct peri_clk_data sdio2_data = {
 };
 
 static struct peri_clk_data sdio3_data = {
+	.policy		= POLICY(0x0010, 3),
 	.gate		= HW_SW_GATE(0x0364, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_52m",
@@ -98,6 +103,7 @@ static struct peri_clk_data sdio3_data = {
 };
 
 static struct peri_clk_data sdio4_data = {
+	.policy		= POLICY(0x0010, 4),
 	.gate		= HW_SW_GATE(0x0360, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_52m",
@@ -110,42 +116,51 @@ static struct peri_clk_data sdio4_data = {
 };
 
 static struct peri_clk_data sdio1_sleep_data = {
+	.policy		= POLICY(0x0010, 6),
 	.clocks		= CLOCKS("ref_32k"),	/* Verify */
 	.gate		= HW_SW_GATE(0x0358, 18, 2, 3),
 };
 
 static struct peri_clk_data sdio2_sleep_data = {
+	.policy		= POLICY(0x0010, 5),
 	.clocks		= CLOCKS("ref_32k"),	/* Verify */
 	.gate		= HW_SW_GATE(0x035c, 18, 2, 3),
 };
 
 static struct peri_clk_data sdio3_sleep_data = {
+	.policy		= POLICY(0x0010, 3),
 	.clocks		= CLOCKS("ref_32k"),	/* Verify */
 	.gate		= HW_SW_GATE(0x0364, 18, 2, 3),
 };
 
 static struct peri_clk_data sdio4_sleep_data = {
+	.policy		= POLICY(0x0010, 4),
 	.clocks		= CLOCKS("ref_32k"),	/* Verify */
 	.gate		= HW_SW_GATE(0x0360, 18, 2, 3),
 };
 
 static struct bus_clk_data sdio1_ahb_data = {
+	.policy		= POLICY(0x0010, 6),
 	.gate		= HW_SW_GATE(0x0358, 16, 0, 1),
 };
 
 static struct bus_clk_data sdio2_ahb_data = {
+	.policy		= POLICY(0x0010, 5),
 	.gate		= HW_SW_GATE(0x035c, 16, 0, 1),
 };
 
 static struct bus_clk_data sdio3_ahb_data = {
+	.policy		= POLICY(0x0010, 3),
 	.gate		= HW_SW_GATE(0x0364, 16, 0, 1),
 };
 
 static struct bus_clk_data sdio4_ahb_data = {
+	.policy		= POLICY(0x0010, 4),
 	.gate		= HW_SW_GATE(0x0360, 16, 0, 1),
 };
 
 static struct bus_clk_data usb_otg_ahb_data = {
+	.policy		= POLICY(0x0010, 11),
 	.gate		= HW_SW_GATE(0x0348, 16, 0, 1),
 };
 
@@ -189,6 +204,7 @@ static struct ccu_data master_ccu_data = {
 /* Slave CCU */
 
 static struct peri_clk_data uartb_data = {
+	.policy		= POLICY(0x0010, 20),
 	.gate		= HW_SW_GATE(0x0400, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_156m",
@@ -199,6 +215,7 @@ static struct peri_clk_data uartb_data = {
 };
 
 static struct peri_clk_data uartb2_data = {
+	.policy		= POLICY(0x0010, 19),
 	.gate		= HW_SW_GATE(0x0404, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_156m",
@@ -209,6 +226,7 @@ static struct peri_clk_data uartb2_data = {
 };
 
 static struct peri_clk_data uartb3_data = {
+	.policy		= POLICY(0x0010, 18),
 	.gate		= HW_SW_GATE(0x0408, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_156m",
@@ -219,6 +237,7 @@ static struct peri_clk_data uartb3_data = {
 };
 
 static struct peri_clk_data bsc1_data = {
+	.policy		= POLICY(0x0010, 24),
 	.gate		= HW_SW_GATE(0x0458, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_104m",
@@ -230,6 +249,7 @@ static struct peri_clk_data bsc1_data = {
 };
 
 static struct peri_clk_data bsc2_data = {
+	.policy		= POLICY(0x0010, 23),
 	.gate		= HW_SW_GATE(0x045c, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_104m",
@@ -241,6 +261,7 @@ static struct peri_clk_data bsc2_data = {
 };
 
 static struct peri_clk_data bsc3_data = {
+	.policy		= POLICY(0x0010, 29),
 	.gate		= HW_SW_GATE(0x0470, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_104m",
@@ -252,6 +273,7 @@ static struct peri_clk_data bsc3_data = {
 };
 
 static struct peri_clk_data bsc4_data = {
+	.policy		= POLICY(0x0010, 30),
 	.gate		= HW_SW_GATE(0x0474, 18, 2, 3),
 	.clocks		= CLOCKS("ref_crystal",
 				 "var_104m",
@@ -263,33 +285,40 @@ static struct peri_clk_data bsc4_data = {
 };
 
 static struct bus_clk_data uartb_apb_data = {
+	.policy		= POLICY(0x0010, 20),
 	.gate		= HW_SW_GATE_AUTO(0x0400, 16, 0, 1),
 };
 
 static struct bus_clk_data uartb2_apb_data = {
+	.policy		= POLICY(0x0010, 19),
 	.gate		= HW_SW_GATE_AUTO(0x0404, 16, 0, 1),
 };
 
 static struct bus_clk_data uartb3_apb_data = {
+	.policy		= POLICY(0x0010, 18),
 	.gate		= HW_SW_GATE_AUTO(0x0408, 16, 0, 1),
 };
 
 static struct bus_clk_data bsc1_apb_data = {
+	.policy		= POLICY(0x0010, 24),
 	.gate		= HW_SW_GATE_AUTO(0x0458, 16, 0, 1),
 	.hyst		= HYST(0x0458, 8, 9),
 };
 
 static struct bus_clk_data bsc2_apb_data = {
+	.policy		= POLICY(0x0010, 23),
 	.gate		= HW_SW_GATE_AUTO(0x045c, 16, 0, 1),
 	.hyst		= HYST(0x045c, 8, 9),
 };
 
 static struct bus_clk_data bsc3_apb_data = {
+	.policy		= POLICY(0x0010, 29),
 	.gate		= HW_SW_GATE_AUTO(0x0470, 16, 0, 1),
 	.hyst		= HYST(0x0470, 8, 9),
 };
 
 static struct bus_clk_data bsc4_apb_data = {
+	.policy		= POLICY(0x0010, 30),
 	.gate		= HW_SW_GATE_AUTO(0x0474, 16, 0, 1),
 	.hyst		= HYST(0x0474, 8, 9),
 };
