901e435f54c77bf3419bb4f2844c78b0b3b83ad2
Artur Weber <aweber.kernel@gmail.com>
2025-08-12 22:10:52 +0200

    mfd: bcm590xx: Add MFD cell for power-on key
    Add MFD cell for the power-on key driver; this allows the power-on key
    to be used.
    
    Signed-off-by: Artur Weber <aweber.kernel@gmail.com>

--- drivers/mfd/bcm590xx.c
+++ drivers/mfd/bcm590xx.c
@@ -32,11 +32,19 @@
 static const struct mfd_cell bcm59054_devs[] = {
 	{ .name = "bcm590xx-vregs", },
 	{ .name = "bcm590xx-poweroff", },
+	{
+		.name = "bcm590xx-ponkey",
+		.of_compatible = "brcm,bcm59054-ponkey",
+	},
 };
 
 static const struct mfd_cell bcm59056_devs[] = {
 	{ .name = "bcm590xx-vregs", },
 	{ .name = "bcm590xx-poweroff", },
+	{
+		.name = "bcm590xx-ponkey",
+		.of_compatible = "brcm,bcm59056-ponkey",
+	},
 };
 
 static bool bcm590xx_volatile_pri(struct device *dev, unsigned int reg)
