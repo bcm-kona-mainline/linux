69c2f64c3ad2125afae5edb938daed735c72eae1
Artur Weber <aweber.kernel@gmail.com>
2026-07-07 19:36:47 +0200

    phy: broadcom: phy-bcm-kona-usb2: Implement full initialization sequence
    Run the full initialization sequence for the USB PHY. This fixes
    USB on some production devices (tested with Samsung Galaxy Grand Neo
    GT-I9060 - DTS not in tree yet).
    
    Signed-off-by: Artur Weber <aweber.kernel@gmail.com>

--- drivers/phy/broadcom/phy-bcm-kona-usb2.c
+++ drivers/phy/broadcom/phy-bcm-kona-usb2.c
@@ -15,20 +15,43 @@
 #include <linux/phy/phy.h>
 #include <linux/platform_device.h>
 
-#define OTGCTL			(0)
+#define OTGCTL			0x00
 #define OTGCTL_OTGSTAT2		BIT(31)
 #define OTGCTL_OTGSTAT1		BIT(30)
+#define OTGCTL_REG_OTGSTAT2	BIT(29)
+#define OTGCTL_REG_OTGSTAT1	BIT(28)
+#define OTGCTL_UTMIOTG_IDDIG_SW	BIT(26)
+#define OTGCTL_PHY_ISO_I	BIT(24)
 #define OTGCTL_PRST_N_SW	BIT(11)
 #define OTGCTL_HRESET_N		BIT(10)
 #define OTGCTL_UTMI_LINE_STATE1	BIT(9)
 #define OTGCTL_UTMI_LINE_STATE0	BIT(8)
+#define OTGCTL_SOFT_LDO_PWRDN	BIT(5)
+#define OTGCTL_SOFT_DLDO_PWRDN	BIT(3)
+#define OTGCTL_SOFT_ALDO_PWRDN	BIT(2)
 
-#define P1CTL			(8)
+#define PHYCFG			0x04
+#define PHYCFG_IDDQ_I		BIT(1)
+
+#define P1CTL			0x08
+#define P1CTL_CLK_REQUEST_CLEAR	BIT(31)
+#define P1CTL_CLK_REQUEST	BIT(30)
 #define P1CTL_SOFT_RESET	BIT(1)
 #define P1CTL_NON_DRIVING	BIT(0)
 
+#define BCCFG				0x10
+#define BCCFG_SW_OVERWRITE_KEY_MASK	GENMASK(31, 17)
+#define BCCFG_SW_OVERWRITE_KEY		0x55560000
+#define BCCFG_SW_OVERWRITE_EN		BIT(16)
+
+#define PHYCTL			0x1c
+#define PHYCTL_SUSPEND		BIT(11)
+
 struct bcm_kona_usb {
 	void __iomem *regs;
+	struct device *dev;
+
+	struct clk *otg_clk;
 };
 
 static void bcm_kona_usb_phy_power(struct bcm_kona_usb *phy, int on)
@@ -50,17 +73,94 @@ static void bcm_kona_usb_phy_power(struct bcm_kona_usb *phy, int on)
 static int bcm_kona_usb_phy_init(struct phy *gphy)
 {
 	struct bcm_kona_usb *phy = phy_get_drvdata(gphy);
+	int ret;
 	u32 val;
+
+	pr_info("bcm_kona_usb_phy_init called\n");
+
+	ret = clk_prepare_enable(phy->otg_clk);
+	if (ret) {
+		dev_err(&gphy->dev, "Failed to enable OTG clock: %d\n", ret);
+	}
+
+	/* Enable software control of PHY-PM */
+	val = readl(phy->regs + OTGCTL);
+	val |= OTGCTL_SOFT_LDO_PWRDN;
+	writel(val, phy->regs + OTGCTL);
 
 	/* Soft reset PHY */
 	val = readl(phy->regs + P1CTL);
-	val &= ~P1CTL_NON_DRIVING;
-	val |= P1CTL_SOFT_RESET;
+	val &= ~P1CTL_SOFT_RESET;
 	writel(val, phy->regs + P1CTL);
-	writel(val & ~P1CTL_SOFT_RESET, phy->regs + P1CTL);
-	/* Reset needs to be asserted for 2ms */
+
+	/* Reset PHY and AHB clock domains */
+	val = readl(phy->regs + OTGCTL);
+	val &= ~(OTGCTL_PRST_N_SW | OTGCTL_HRESET_N);
+	writel(val, phy->regs + OTGCTL);
+
+	/* Deassert clock domain reset */
+	val = readl(phy->regs + OTGCTL);
+	val |= OTGCTL_PRST_N_SW | OTGCTL_HRESET_N;
+	writel(val, phy->regs + OTGCTL);
 	mdelay(2);
+
+	/* Power up ALDO/DLDO */
+	val = readl(phy->regs + OTGCTL);
+	val |= OTGCTL_SOFT_ALDO_PWRDN | OTGCTL_SOFT_DLDO_PWRDN;
+	writel(val, phy->regs + OTGCTL);
+	mdelay(1);
+
+	/* Enable pad, internal PLL */
+	val = readl(phy->regs + PHYCFG);
+	val &= ~PHYCFG_IDDQ_I;
+	writel(val, phy->regs + PHYCFG);
+
+	/* Set LDO suspend mask */
+	val = readl(phy->regs + PHYCTL);
+	val |= PHYCTL_SUSPEND;
+	writel(val, phy->regs + PHYCTL);
+
+	/* Remove PHY isolation */
+	val = readl(phy->regs + OTGCTL);
+	val &= ~OTGCTL_PHY_ISO_I;
+	writel(val, phy->regs + OTGCTL);
+	mdelay(1);
+
+	/* PHY clock reset */
+	val = readl(phy->regs + P1CTL);
+	val |= P1CTL_CLK_REQUEST;
+	writel(val, phy->regs + P1CTL);
+
+	val = readl(phy->regs + P1CTL);
+	val |= P1CTL_CLK_REQUEST_CLEAR;
+	writel(val, phy->regs + P1CTL);
+	mdelay(2);
+
+	/* Bring PHY out of reset state */
 	writel(val | P1CTL_SOFT_RESET, phy->regs + P1CTL);
+
+	/* Set correct ID value */
+	val = readl(phy->regs + OTGCTL);
+	val |= OTGCTL_UTMIOTG_IDDIG_SW;
+	writel(val, phy->regs + OTGCTL);
+
+	/* Set VBUS valid state */
+	val = readl(phy->regs + OTGCTL);
+	val |= OTGCTL_REG_OTGSTAT1 | OTGCTL_REG_OTGSTAT2;
+	writel(val, phy->regs + OTGCTL);
+	mdelay(200);
+
+	/* Enable SW overwrite */
+	val = readl(phy->regs + BCCFG);
+	val &= ~BCCFG_SW_OVERWRITE_KEY_MASK;
+	val |= (BCCFG_SW_OVERWRITE_KEY | BCCFG_SW_OVERWRITE_EN);
+	writel(val, phy->regs + BCCFG);
+	mdelay(2);
+
+	/* Clear non-driving bit */
+	val = readl(phy->regs + P1CTL);
+	val &= ~P1CTL_NON_DRIVING;
+	writel(val, phy->regs + P1CTL);
 
 	return 0;
 }
@@ -68,6 +168,8 @@ static int bcm_kona_usb_phy_init(struct phy *gphy)
 static int bcm_kona_usb_phy_power_on(struct phy *gphy)
 {
 	struct bcm_kona_usb *phy = phy_get_drvdata(gphy);
+
+	pr_info("bcm_kona_usb_phy_power_on called\n");
 
 	bcm_kona_usb_phy_power(phy, 1);
 
@@ -77,6 +179,8 @@ static int bcm_kona_usb_phy_power_on(struct phy *gphy)
 static int bcm_kona_usb_phy_power_off(struct phy *gphy)
 {
 	struct bcm_kona_usb *phy = phy_get_drvdata(gphy);
+
+	pr_info("bcm_kona_usb_phy_power_off called\n");
 
 	bcm_kona_usb_phy_power(phy, 0);
 
@@ -104,6 +208,11 @@ static int bcm_kona_usb2_probe(struct platform_device *pdev)
 	phy->regs = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(phy->regs))
 		return PTR_ERR(phy->regs);
+
+	phy->otg_clk = devm_clk_get(dev, "otg");
+	if (IS_ERR(phy->otg_clk))
+		return dev_err_probe(dev, PTR_ERR(phy->otg_clk),
+				     "Failed to get OTG clock\n");
 
 	platform_set_drvdata(pdev, phy);
 
